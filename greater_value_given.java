176c7ca2cde95b93ff2e937ddb476b61e8627da5
apatil851 <95017463+apatil851@users.noreply.github.com>
2021-11-26 18:23:32 +0530

    Add files via upload
    home work

---
+++ greater_value_given.java
@@ -0,0 +1,16 @@
+
+public class greater_value_given {
+	public static void main(String[] args) {
+
+	      int num1 = 10, num2 = 20, num3 = 7;
+
+	      if( num1 >= num2 && num1 >= num3)
+	          System.out.println(num1);
+
+	      else if (num2 >= num1 && num2 >= num3)
+	          System.out.println(num2);
+
+	      else
+	          System.out.println(num3);
+	  }
+}
